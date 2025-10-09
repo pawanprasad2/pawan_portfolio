6429c299a042892e3520ee73bebfc0ab8fa7d8f9
pawan <pawanprasad2322@gmail.com>
2025-10-10 04:10:25 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { eslint: {
-    ignoreDuringBuilds: true,
+const nextConfig = {
+  output: 'export',  // This makes it static
+  images: {
+    unoptimized: true,  // Required for static export
   },
-  output: 'standalone', // important for Amplify
-};
+}
 
+module.exports = nextConfig

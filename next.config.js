36788481dd92fd62337d9c1294d6f7bf25d8d15f
pawan <pawanprasad2322@gmail.com>
2025-10-10 03:36:17 +0530

    bugs

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',  // Required for Amplify
-  reactStrictMode: true, // Optional, but recommended
-}
-
-module.exports = nextConfig

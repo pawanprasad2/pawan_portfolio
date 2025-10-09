baa1bd8925dacf7836ab7bdb289f753688dc2428
pawan <pawanprasad2322@gmail.com>
2025-10-10 04:13:15 +0530

    mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // This makes it static
+  output: 'export',
   images: {
-    unoptimized: true,  // Required for static export
+    unoptimized: true,
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
